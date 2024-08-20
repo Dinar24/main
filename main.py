695a5c37a36ce25c2203d7ca79db3188fb6ab936
Dinar24 <dinar.nurgajzov@yandex.ru>
2024-08-21 01:13:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-# "1st program"
-print(9**0.5*5)
-# "2nd program"
-print(9.99 > 9.98 and 1000 != 1000.1)
-# "3rd program"
-print(1234 % 1000 // 10 + 5678 % 1000 // 10)
-# "4th program"
-print(int(13.42) == int(42.13*100) % 100 == int(42.13))
+from fake_math import *
+from true_math import *
+
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
