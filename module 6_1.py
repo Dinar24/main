19d217c28b8007de23a07688b91c2dd7b6006e8d
Dinar  Nurgajzov <dinar.nurgajzov@yandex.ru>
2024-09-14 15:59:51 +0300

    Add files via upload

--- module 6_1.py
+++ module 6_1.py
@@ -1,45 +1,64 @@
+# Родительский класс Animal
 class Animal:
-    alive = True
-    fed = False
     def __init__(self, name):
-        self.name = name
+        self.alive = True   # Живое по умолчанию
+        self.fed = False    # Не накормлено по умолчанию
+        self.name = name    # Имя животного
+
+    def eat(self, food):
+        if isinstance(food, Plant):
+            if food.edible:
+                print(f"{self.name} съел {food.name}")
+                self.fed = True  # Животное накормлено
+            else:
+                print(f"{self.name} не стал есть {food.name}")
+                self.alive = False  # Животное погибло
+        else:
+            print(f"{self.name} не может есть {food}")
+
+# Родительский класс Plant
 class Plant:
-    edible = False
     def __init__(self, name):
-        self.name = name
+        self.edible = False  # Не съедобно по умолчанию
+        self.name = name     # Имя растения
+
+# Класс Mammal, наследник от Animal
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible == True:
-            Animal.fed = True
-            print(f'{self.name} съел {food.name}')
-        else:
-            Animal.alive = False
-            print(f'{self.name} не стал есть {food.name}')
+    pass  # Другие специфические атрибуты и методы можно добавить здесь
+
+# Класс Predator, наследник от Animal
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible == True:
-            Animal.fed = True
-            print(f'{self.name} съел {food.name}')
-        else:
-            Animal.alive = False
-            print(f'{self.name} не стал есть {food.name}')
+    pass  # Другие специфические атрибуты и методы можно добавить здесь
+
+# Класс Flower, наследник от Plant
 class Flower(Plant):
-    edible = False
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = False  # Цветок несъедобен
 
+# Класс Fruit, наследник от Plant
 class Fruit(Plant):
-    edible = True
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True  # Фрукт съедобен
 
+# Тестирование
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
-p2 = Fruit('Завадной апельсин')
+p2 = Fruit('Заводной апельсин')
 
+# Вывод информации
 print(a1.name)
 print(p1.name)
 
 print(a1.alive)
 print(a2.fed)
-a1.eat(p1)
-a2.eat(p2)
+
+# Попытка поесть
+a1.eat(p1)  # Волк с Уолл-Стрит пытается съесть Цветик семицветик (несъедобно)
+a2.eat(p2)  # Хатико съедает Заводной апельсин (съедобно)
+
+# Состояние после еды
 print(a1.alive)
 print(a2.fed)
