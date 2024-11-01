634d78be658c5f6000d1925db57cf8dde9f2695f
Dinar  Nurgajzov <dinar.nurgajzov@yandex.ru>
2024-11-02 00:20:35 +0300

    Update module_11_3.py

--- module_11_3.py
+++ module_11_3.py
@@ -1,62 +1,29 @@
-# Домашнее задание по теме "Интроспекция"
-import inspect
-from pprint import pprint
+def introspection_info(obj):
+    obj_type = type(obj).__name__
+
+    # Получение атрибутов объекта
+    attributes = [at for at in dir(obj) if not callable(getattr(obj, at))]
+
+    # Получение методов объекта
+    methods = [m for m in dir(obj) if callable(getattr(obj, m))]
+
+    # Получение информации о модуле
+    module = obj.__class__.__module__
+
+    dict_int = {
+        'type': obj_type,
+        'attributes': attributes,
+        'methods': methods,
+        'module': module,
+
+    }
+
+    return dict_int
 
 
-class Class1():
-    at = 1
 
-    def test(x):
-        pass
+number_info = introspection_info(42)
+print(number_info)
 
-
-def introspection_info(obj=None, **kwargs):
-    info_dic: dict = {}
-    try:
-        print(f'\n\033[93mИсследуем объект {obj.__name__}\033[0m')
-    except AttributeError:
-        print(f'\n\033[93mИсследуем объект {obj}\033[0m')
-    print('Тип объекта:', type(obj), sep='\n\t')
-    info_dic['type'] = type(obj)
-    print('Атрибуты объекта:', dir(obj), sep='\n\t')
-    info_dic['attributes'] = dir(obj)
-    method_list = []
-    for attr_name in dir(obj):
-        attr = getattr(obj, attr_name)
-        print(f'Имя: {attr_name:<20} Тип: {str(type(attr)):<40} Исполняемый: {callable(attr)}')
-        if callable(attr):
-            method_list.append(attr_name)
-    info_dic['methods'] = method_list
-    print('Модуль, к которому объект принадлежит:', inspect.getmodule(obj), sep='\n\t')
-    info_dic['module'] = inspect.getmodule(obj)
-    if inspect.isfunction(obj):
-        sign = inspect.signature(obj)
-        print('Передаваемый в функцию параметры:', sign.parameters, sep='\n\t')
-        param_list = []
-        for p_name, param in sign.parameters.items():
-            param_list.append([param.name, param.default])
-        info_dic['signature'] = param_list
-        print('-' * 50)
-    return info_dic
-
-
-info = introspection_info(42)
-print()
-pprint(info)
-
-info = introspection_info(introspection_info)
-print()
-pprint(info)
-
-info = introspection_info(Class1)
-print()
-pprint(info)
-
-obj_class1 = Class1()
-info = introspection_info(obj_class1)
-print()
-pprint(info)
-
-info = introspection_info()
-print()
-pprint(info)
+string_info = introspection_info("пример")
+print(string_info)
