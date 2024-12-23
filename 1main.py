12cfa7e2076c20f8772bccd709e401e8c3ea8c55
Dinar  Nurgajzov <dinar.nurgajzov@yandex.ru>
2024-12-24 01:22:08 +0300

    Add files via upload

--- 1main.py
+++ 1main.py
@@ -4,19 +4,21 @@ app = FastAPI()
 
 
 @app.get('/user/admin')
-async def welcom():
+async def call_admin():
     return {'message': 'Вы вошли как администратор'}
 
 @app.get('/user/{user_id}')
-async def welcom(user_id: int):
+async def call_id(user_id: int):
     return {'message': f'Вы вошли как пользователь № {user_id}'}
 
 
 @app.get('/user')
-async def welcom(username: str, age: int):
+async def call_user(username: str, age: int):
     return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
 
 
 @app.get('/')
-async def welcom():
+async def call():
     return {'message': 'Главная страница'}
+
+# uvicorn main2:app --reload
