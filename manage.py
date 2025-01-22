7113b2d7c26f25c5c1a82dd51925795c56e7a81e
Dinar  Nurgajzov <dinar.nurgajzov@yandex.ru>
2025-01-23 01:06:01 +0300

    Add files via upload

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""UrbanDjango's command-line utility for administrative tasks."""
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
@@ -11,7 +11,7 @@ def main():
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import UrbanDjango. Are you sure it's installed and "
+            "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
@@ -20,3 +20,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# Python manage.py makemigrations
+# Python manage.py migrate
+# Python manage.py runserver
